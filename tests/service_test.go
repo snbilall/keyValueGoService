f7d37da9a7107a1c7c7e875a988315dfc69c6f48
snbilall <abdurrehimsen@gmail.com>
2021-12-11 14:56:05 +0300

    tests file is ok

--- tests/service_test.go
+++ tests/service_test.go
@@ -9,17 +9,24 @@ import (
 	"keyValueProject/service"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
-func TestSuccessAddKeyValueService(t *testing.T) {
+var testRouter *gin.Engine
+
+func TestMain(m *testing.M) {
 	service.Init()
-	testRouter := routers.SetupRouter()
-	key := "Sean"
-	value := "Paul"
-	body := models.KeyValueRequest{Key: key, Value: value}
+	testRouter = routers.SetupRouter()
+	os.Exit(m.Run())
+}
+
+func TestSuccessAddKeyValueService(t *testing.T) {
+	obj := models.KeyValueRequest{Key: "Sean", Value: "Paul"}
 	var buf bytes.Buffer
-	err := json.NewEncoder(&buf).Encode(body)
+	err := json.NewEncoder(&buf).Encode(obj)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -31,17 +38,14 @@ func TestSuccessAddKeyValueService(t *testing.T) {
 	resp := httptest.NewRecorder()
 	testRouter.ServeHTTP(resp, req)
 	if resp.Code != 201 {
-		t.Fatalf(`adding key %s, value %s service test failed`, key, value)
+		t.Fatalf(`adding key %s, value %s service test failed`, obj.Key, obj.Value)
 	}
 }
 
 func TestUnsuccessAddKeyValueService(t *testing.T) {
-	service.Init()
-	testRouter := routers.SetupRouter()
-	value := "Paul"
-	body := models.KeyValueRequest{Value: value}
+	obj := models.KeyValueRequest{Value: "Paul"}
 	var buf bytes.Buffer
-	err := json.NewEncoder(&buf).Encode(body)
+	err := json.NewEncoder(&buf).Encode(obj)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -53,6 +57,35 @@ func TestUnsuccessAddKeyValueService(t *testing.T) {
 	resp := httptest.NewRecorder()
 	testRouter.ServeHTTP(resp, req)
 	if resp.Code != 400 {
-		t.Fatalf(`adding empty key, value %s service test failed`, value)
+		t.Fatalf(`adding empty key, value %s service test failed`, obj.Value)
+	}
+}
+
+func TestSuccessGetKeyValueService(t *testing.T) {
+	obj := models.KeyValueRequest{Key: "Paul", Value: "Scholes"}
+	service.AddKeyValue(obj)
+	req, err := http.NewRequest(http.MethodGet, "/keyValues/"+obj.Key, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp := httptest.NewRecorder()
+	testRouter.ServeHTTP(resp, req)
+	if resp.Code != 200 {
+		t.Fatalf(`get success key %s, value %s service test failed`, obj.Key, obj.Value)
+	}
+}
+
+func TestUnsuccessGetKeyValueService(t *testing.T) {
+	key := "Herald"
+	req, err := http.NewRequest(http.MethodGet, "/keyValues/"+key, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp := httptest.NewRecorder()
+	testRouter.ServeHTTP(resp, req)
+	if resp.Code != 404 {
+		t.Fatalf(`get unsucess key %s service test failed`, key)
 	}
 }
